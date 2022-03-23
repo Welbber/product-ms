2a8a349cbb1fe4b860047ff8604c4062db71cc22
welbber <welbervital01@gmail.com>
2022-03-23 14:45:01 -0300

    Criação da entidade product

---
+++ src/main/java/uol/compass/desafiojavaspringboot/productms/entity/product.java
@@ -0,0 +1,32 @@
+package uol.compass.desafiojavaspringboot.productms.entity;
+
+import lombok.*;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Table(name = "Products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.PRIVATE)
+    private Long id;
+
+    @NotBlank(message = "The name field needs to be filled")
+    private String name;
+
+    @NotBlank(message = "The description field needs to be filled")
+    private String description;
+
+    @NotBlank(message = "The price field needs to be filled")
+    @Min(0)
+    private Double price;
+}
