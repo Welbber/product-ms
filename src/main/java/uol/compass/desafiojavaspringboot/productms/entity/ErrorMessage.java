775e1a9a6f8f8801b90bcd069443378adb66ae25
welbber <welbervital01@gmail.com>
2022-03-28 20:58:31 -0300

    aplicação JsonProperty objeto ErrorMessage

--- src/main/java/uol/compass/desafiojavaspringboot/productms/entity/ErrorMessage.java
+++ src/main/java/uol/compass/desafiojavaspringboot/productms/entity/ErrorMessage.java
@@ -1,5 +1,6 @@
 package uol.compass.desafiojavaspringboot.productms.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class ErrorMessage {
+    @JsonProperty("status_code")
     private Integer statusCode;
     private String message;
 }
